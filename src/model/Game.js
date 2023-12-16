98753c8cc7e5494d55944fe145b1201516472013
Th3Ro <nosolencko@gmail.com>
2023-12-16 14:42:05 +0300

    Added Book, Movie, Game models and connect to mongo

---
+++ src/model/Game.js
@@ -0,0 +1,18 @@
+const {Schema, model} = require('mongoose');
+
+const Game = new Schema({
+    title: {type: String, unique: false, required: true},
+    description: {type: String, unique: false, required: true},
+    img_url: {type: String, unique: false, required: false},
+    metcrt_rating: {type: Number, unique: false, required: false},
+    developers: {type: String, unique: false, required: false},
+    release_date: {type: Date, unique: false, required: false},
+    genres: {type: String, unique: false, required: true},
+    screenshot_url: {type: String, unique: false, required: false},
+    pc_req_min: {type: String, unique: false, required: false},
+    pc_req_rec: {type: String, unique: false, required: false},
+    video: {type: String, unique: false, required: false},
+    user_rating: {type: Number, unique: false, required: false}
+})
+
+module.exports = model('Game', Game);
