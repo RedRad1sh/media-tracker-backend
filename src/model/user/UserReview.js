ab89b32dd15a670ed53f0db0b1e767e57b955b3b
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2023-12-25 01:19:16 +0300

    fix

--- src/model/user/UserReview.js
+++ src/model/user/UserReview.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose');
 const mongoosePaginate = require("mongoose-paginate-v2");
 
-const UserReviews = new Schema({
+const UserReview = new Schema({
     user_id: {
         type: Schema.Types.ObjectId,
         ref: 'User',
@@ -11,7 +11,7 @@ const UserReviews = new Schema({
     content_id: {type: String, unique: false, required: true},
     action: {type: Date, unique: false, required: true}
 })
-UserLists.index({ user_id: 1, content_id: 1}, { unique: true });
-UserReviews.plugin(mongoosePaginate);
+UserReview.index({ user_id: 1, content_id: 1}, { unique: true });
+UserReview.plugin(mongoosePaginate);
 
-module.exports = model('UserReviews', UserReviews);
+module.exports = model('UserReview', UserReview);
