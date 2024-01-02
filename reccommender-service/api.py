58fcf57961a00d5be9f6bdb641ead447d6b6daef
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2024-01-02 21:27:55 +0300

    добавлена джоба для обновления кеша с бд

--- reccommender-service/api.py
+++ reccommender-service/api.py
@@ -1,12 +1,16 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request
 from content_based import recommend_content
+import schedule
+from db_cache_update import updateJob
+import time
+from threading import Thread
 
 app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify({'error': 'Not found'}), 404
 
 @app.route('/recommend/simple', methods=['POST'])
 def recommend_simple():
@@ -20,5 +24,15 @@ def recommend_simple():
     result = recommend_content(recommend_object)
     return jsonify(result), 201
 
+def run_jobs():
+
+    while True: 
+        schedule.run_pending() 
+        time.sleep(1)
+
 if __name__ == '__main__':
+    schedule.every(1).hours.do(updateJob) 
+    thread = Thread(target=run_jobs)
+    thread.daemon = True
+    thread.start()
     app.run(debug=True)
