b4ca9ddc18733043f33d3f3fc77fcaa996a0d4bb
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2024-01-08 18:58:08 +0300

    fix rebase

--- reccommender-service/api.py
+++ reccommender-service/api.py
@@ -1,5 +1,4 @@
 #!flask/bin/python
-<<<<<<< HEAD
 from flask import Flask, jsonify, request, abort
 from content_based import recommend_content
 import schedule
@@ -9,16 +8,11 @@ from threading import Thread
 import pandas as pd
 import json
 
-=======
-from flask import Flask, jsonify, request
-from content_based import recommend_content
->>>>>>> b95a42b (Реализованы простые рекомендации на основе разницы в указанных полях бд (фильм - фильм, игра - игра, книга - книга))
 
 app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(error):
-<<<<<<< HEAD
     return jsonify({'error': 'Not found'}), 404
 
 @app.route('/recommend/simple', methods=['POST'])
@@ -47,28 +41,4 @@ if __name__ == '__main__':
     thread = Thread(target=run_jobs)
     thread.daemon = True
     thread.start()
-=======
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-@app.route('/recommend/simple', methods=['POST'])
-def recommend_simple():
-    if not request.json or not 'content_ids' in request.json:
-        abort(400)
-    recommend_object = {
-        'content_ids': request.json['content_ids'],
-        'content_type': request.json.get('content_type', ""),
-        'reccommend_content_type': request.json.get('reccommend_content_type', "")
-    }
-    print(recommend_object)
-    result = recommend_content(recommend_object)
-    return jsonify(result), 201
-
-def run_jobs():
-
-    while True: 
-        schedule.run_pending() 
-        time.sleep(1)
-
-if __name__ == '__main__':
->>>>>>> b95a42b (Реализованы простые рекомендации на основе разницы в указанных полях бд (фильм - фильм, игра - игра, книга - книга))
     app.run(debug=True)
--- reccommender-service/content_based.py
+++ reccommender-service/content_based.py
@@ -1,7 +1,6 @@
 # импорт библиотек
 import pandas as pd
 import numpy as np
-<<<<<<< HEAD
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from db_cache_update import load_df
@@ -20,26 +19,11 @@ features = {
 }
 
 content_type_to_plural = {
-=======
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from db_cache_update import load_df
-
-from multi_rake import Rake
-
-# df_1 = pd.read_csv("movies.csv")
-movies_features = ['actors','genres','directors']
-games_features = ['title', 'genres']
-books_features = ['title','categories','authors']
-
-content_type_to_mongo_collection = {
->>>>>>> b95a42b (Реализованы простые рекомендации на основе разницы в указанных полях бд (фильм - фильм, игра - игра, книга - книга))
     "Movie": "movies",
     "Game": "games",
     "Book": "books"
 }
 
-<<<<<<< HEAD
 def recommend_content(recommend_object):
     prepare_df_func = {
         "Movie": prepare_movies_df,
@@ -142,35 +126,6 @@ def get_keywords(text):
     keyword_str = ' '.join(map(lambda x: x[0],keywords[:5]))
     return keyword_str
 
-=======
-def load_df(content_type):
-    # Кеш фрейма, чтобы все время в бд не ходить
-    # Обновление кеша нужно перенести в асинхронную джобу
-    collection = content_type_to_mongo_collection[content_type]
-    file_path = f"./{collection}.pkl"
-    try:
-        create_time = os.path.getmtime(file_path)
-        current_time = time.time()
-        print(f'Время жизни кеша: {current_time - create_time}')
-        # 60 * 1 - по умолчанию обновляется раз в час
-        if current_time - create_time < 60 * 60:
-            df_1 = pd.read_pickle(f'{collection}.pkl')
-            return df_1
-    except:
-        print('Ошибка при загрузке кеша - будет создан новый')
-
-    df_1 = read_mongo(collection)
-    df_1.to_pickle(f'{collection}.pkl')
-    return df_1;
-
-
-def combine_features(row, features):
-    result = ""
-    for feature in features:
-        result += row[feature] + ' '
-    return result
-
->>>>>>> b95a42b (Реализованы простые рекомендации на основе разницы в указанных полях бд (фильм - фильм, игра - игра, книга - книга))
 def get_const_content_id_from_index(df_1, index):
     return df_1['const_content_id'][index]
 
@@ -180,96 +135,16 @@ def get_index_from_const_content_id(df_1, const_content_id):
 def get_title_from_index(df_1, index):
     return df_1['title'][int(index)]
 
-<<<<<<< HEAD
 def get_reccomendation_ids_list(df_1, sorted_similar, indexes_to_remove):
     i=0
-=======
-def recommend_content(recommend_object):
-    prepare_df_func = {
-        "Movie": prepare_movies_df,
-        "Game": prepare_games_df,
-        "Book": prepre_books_df
-    }
-    
-    reccommend_content_type = recommend_object['reccommend_content_type']
-    df_1 = load_df(reccommend_content_type)
-    prepare_df_func[reccommend_content_type](df_1)
-    content_types_for_recommend = recommend_object['content_types_for_recommend']
-    concatenated_user_lists = reduce(lambda x, y: recommend_object[x] + recommend_object[y], content_types_for_recommend) if len(content_types_for_recommend) > 1 else recommend_object[content_types_for_recommend[0]]
-    user_lists_df = pd.read_json(json.dumps(concatenated_user_lists))
-    
-    if (len(content_types_for_recommend) == 1):
-        prepare_df_func[content_types_for_recommend[0]](user_lists_df)
-    else:
-        concatenated_features = reduce(lambda x, y: features[x] + features[y], content_types_for_recommend) if len(content_types_for_recommend) > 1 else features[content_types_for_recommend[0]]
-        prepare_combined_features(user_lists_df, concatenated_features)
-    
-    print(user_lists_df['combined_features'][0]) 
-    
-    recommend_result = create_recommend_list_cross(df_1, user_lists_df)
-        
-    sorted_similar = recommend_result[0]
-    indexes_to_remove = recommend_result[1]
-
-    i=1
->>>>>>> b95a42b (Реализованы простые рекомендации на основе разницы в указанных полях бд (фильм - фильм, игра - игра, книга - книга))
     result = []
     for element in sorted_similar:
         if (element[0] not in indexes_to_remove):
             result.append(str(get_const_content_id_from_index(df_1, element[0])))
             print(get_title_from_index(df_1, element[0]))
             i=i+1
-<<<<<<< HEAD
             if i>20:
                 break
         else:
             print('')
     return { 'ids': result }
-=======
-            if i>10:
-                break
-        else:
-            print('')
-    return { 'ids': result }
-
-def create_recommend_list(df_1, recommend_object):
-    cv = TfidfVectorizer()
-    count_matrix = cv.fit_transform(df_1['combined_features'])
-    cosine_sim = cosine_similarity(count_matrix)
-
-    user_lists = recommend_object['content_ids']
-    similar = list()
-    indexes_to_remove = list(map(lambda item: get_index_from_const_content_id(df_1, item)[0], user_lists))
-
-    for content in user_lists:
-        content_index = get_index_from_const_content_id(df_1, content)[0]
-        similar = similar + list(enumerate(cosine_sim[content_index]))
-
-    sorted_similar = sorted(similar,key=lambda x:x[1],reverse=True)[1:]
-    return [sorted_similar, indexes_to_remove]
-
-
-# Методы подготовки датасетов
-
-def prepare_combined_features(df, features_to_combine):
-    for feature in features_to_combine:
-        df[feature] = df[feature].fillna('') #filling all NaNs with blank string
-    df['combined_features'] = df.apply(combine_features,args=(features_to_combine,'description',),axis=1, )
-
-
-def prepare_movies_df(df_1):
-    df_1['kp_rating'] = df_1['kp_rating'].fillna(df_1['kp_rating'].mean()) 
-    prepare_combined_features(df_1, features['Movie'])
-
-def prepare_games_df(df_1):
-    df_1['metcrt_rating'] = df_1['metcrt_rating'].fillna(df_1['metcrt_rating'].mean()) 
-    prepare_combined_features(df_1, features['Game'])
-
-def prepre_books_df(df_1):
-    df_1['user_rating'] = df_1['user_rating'].fillna(df_1['user_rating'].mean()) 
-
-    for feature in books_features:
-        df_1[feature] = df_1[feature].fillna('') #filling all NaNs with blank string
-    df_1['combined_features'] = df_1.apply(combine_features,args=(books_features,'description',),axis=1)
-    return create_recommend_list(df_1, recommend_object)
->>>>>>> b95a42b (Реализованы простые рекомендации на основе разницы в указанных полях бд (фильм - фильм, игра - игра, книга - книга))
