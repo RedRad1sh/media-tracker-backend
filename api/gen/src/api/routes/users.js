9b9e6e500df60981b68fb58e1ebdd680648fed49
Th3Ro <nosolencko@gmail.com>
2024-01-09 14:17:54 +0300

    Добавлена авторизация и необходимость авторизации для получения данных по спискам и рецензиям

--- api/gen/src/api/routes/users.js
+++ api/gen/src/api/routes/users.js
@@ -32,9 +32,9 @@ router.get('/:id', async (req, res, next) => {
  * Тело:
  * 
  * @param {String} login
- * @param {String} password (movies, games, books)
+ * @param {String} password
  * @param {String} email
- * @param {String} imgUrl (строка Запланировано, Просмотрено и т.п.)
+ * @param {String} imgUrl
  */
 router.post('/', async (req, res, next) => {
   const options = {
@@ -43,6 +43,28 @@ router.post('/', async (req, res, next) => {
 
   try {
     const result = await users.createUser(options);
+    res.status(result.status || 200).send(result.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * Авторизация пользователя
+ *
+ * Тело:
+ *
+ * @param {String} id
+ * @param {String} login
+ * @param {String} password
+ */
+router.post('/auth', async (req, res, next) => {
+  const options = {
+    requestBody: req.body
+  };
+
+  try {
+    const result = await users.auth(options);
     res.status(result.status || 200).send(result.data);
   } catch (err) {
     next(err);
